7a359e0c22a7ce10f4df855bde60ceac58d96216
jinrunheng <1175088275@qq.com>
2024-02-22 23:15:11 +0800

    字符串的常用操作

---
+++ py_lesson_09_字符串的常用操作/chinese_zodiac.py
@@ -0,0 +1,15 @@
+# 记录生肖，根据年份来判断生肖
+chinese_zodiac = "鼠牛虎兔龙蛇马羊猴鸡狗猪"
+
+if "狗" in chinese_zodiac:
+    print("狗在十二生肖序列中")
+
+if "猫" not in chinese_zodiac:
+    print("猫不在十二生肖序列中")
+
+str1 = "123"
+str2 = "456"
+str3 = str1 + str2
+print(str3)
+# 重复三次
+print(str3 * 3)
